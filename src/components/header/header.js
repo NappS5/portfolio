444ed9cfb81cf4e2e7630c200c117f61d899e921
Felipe Sander Dhein <felipesanderd2@gmail.com>
2023-10-28 21:25:03 -0300

    first commit

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+function Header(){
+    return(
+        <>
+        <div className='container'>
+            <h1 className='name'>FELIPE SANDER DHEIN</h1>
+            <p className='about-me'>20 anos | 14/04/2003  | Brasil </p>
+        </div>
+        </>
+
+    )
+}
+
+export default Header
---
+++ src/components/me/me.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './me.css'
+
+function Me(){
+    return(
+        <div className='me-container'>
+            <img className='felipe-sander' src='./imgs/felipe-sander.png' alt='Me similing'></img>
+        </div>
+    )
+}
+
+export default Me
