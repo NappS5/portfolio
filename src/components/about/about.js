b5a8d572dc4e785ef9e009fcc377cd4947729018
Felipe Sander Dhein <felipesanderd2@gmail.com>
2023-10-31 17:31:24 -0300

    aa

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,22 +4,31 @@ import './about.css'
 function About() {
     return (
         <>
-        <div className='about-container'>
-        <div className='about_me'>
-            <h1 className='title'>Sobre mim</h1>
-            <p className='description'>Visual Studio Code is the most used code editor amongst developers these days. Git is an open-source version control system. It means that whenever a developer develops some project (like an app or website) or something, he/she constantly updates it catering to the demands of users, technology, and whatsoever it maybe, Git is a version control system that lets you manage and keep track of your source code history. Find the appropriate web project according to your requirement in GitHub. In this article, we will see how to clone the existing projects from GitHub using VSCode IDE.</p>
-        </div>
+            <div className='about-container'>
+                <div className='about_me'>
+                    <h1 className='title'>Sobre mim</h1>
+                    <p className='description'>Meu nome é Felipe Sander Dhein e tenho 20 anos. Comecei na programção em sí não faz muito tempo,
+                        me interessei pelo desenvolvimento WEB e hoje estou em busca de ser um desenvolvedor Fullstack, desde cedo sempre gostei da área
+                        de técnologia, e a curiosidade e amor pelos desafios proporcionados por essa área só intensificaram mais ainda meu desejo de trabalhar
+                        na área. Sei que posso ser de muito valor para aqueles que trabalham junto de mim, pois aprendo muito rápido, tendo também um forte
+                        espírito de liderança. Sei me virar muito bem diante das adversidades e me considero um eterno aprendiz, aprendendo todos os dias, com tudo e todos.  </p>
+                </div>
 
-        <div className='about_me'>
-            <h1 className='title'>Objetivo</h1>
-            <p className='description'>Visual Studio Code is the most used code editor amongst developers these days. Git is an open-source version control system. It means that whenever a developer develops some project (like an app or website) or something, he/she constantly updates it catering to the demands of users, technology, and whatsoever it maybe, Git is a version control system that lets you manage and keep track of your source code history. Find the appropriate web project according to your requirement in GitHub. In this article, we will see how to clone the existing projects from GitHub using VSCode IDE.</p>
-        </div>
-        <div className='about_me'>
-            <h1 className='title'>Habilidades</h1>
-            <p className='description'>Visual Studio Code is the most used code editor amongst developers these days. Git is an open-source version control system. It means that whenever a developer develops some project (like an app or website) or something, he/she constantly updates it catering to the demands of users, technology, and whatsoever it maybe, Git is a version control system that lets you manage and keep track of your source code history. Find the appropriate web project according to your requirement in GitHub. In this article, we will see how to clone the existing projects from GitHub using VSCode IDE.</p>
-        </div>
-        </div>
-        
+                <div className='about_me'>
+                    <h1 className='title'>Objetivo</h1>
+                    <p className='description'>Meu objetivo pessoal é somar aos outros da melhor forma que eu puder sempre, atuando e 
+                    aprendendo com todas as oportunidades, é poder ter, nem que seja um pouco da minha ajuda, no máximo de projetos possíveis,
+                    é fazer o meu nome junto das empresas as quais estarei trabalhando. Sou muito ambicioso e estou disposto a fazer de tudo pelo
+                    meu objetivo, sendo assim de grande adição para a empresa na qual estarei trabalhando, pois darei o meu melhor para atingir aquilo que almejo.</p>
+                </div>
+                <div className='about_me'>
+                    <h1 className='title'>Habilidades</h1>
+                    <p className='description'>Além das minhas habilidades em Javascript, HTML, CSS, React.js, Node.js, também tenho muita facilidade em
+                    liderança, podendo trabalhar junto de grandes equipes em busca de um mesmo objetivo, tenho facilidade na comunicação com aqueles que estão tanto acima,
+                    quanto "abaixo" de mim. Sou muito atencioso e paciente com colegas e clientes, tendo humor alegre e cooperativo.</p>
+                </div>
+            </div>
+
         </>
     )
 }
