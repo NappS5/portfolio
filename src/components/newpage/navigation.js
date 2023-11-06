ecd26585bf1147283f93f67ddbabe43df9ecf865
Felipe Sander Dhein <felipesanderd2@gmail.com>
2023-11-06 14:05:35 -0300

    new build

--- src/components/newpage/navigation.js
+++ src/components/newpage/navigation.js
@@ -1,22 +1,21 @@
-// navigation.js
-import { Link } from 'react-router-dom';
-import './navigation.css'
+// // navigation.js
+// import { Link } from 'react-router-dom';
 
-export function Navigation() {
-  return (
-    <>
-    <div className='navigation-container'>
-    <nav>
-      <ul>
-        <li className='paginas'>
-          <Link className='sobre-mim' to="/">Sobre mim</Link>
-        </li>
-        <li>
-          <Link className='portfolio' to="/portfolio">Portfólio</Link>
-        </li>
-      </ul>
-    </nav>
-    </div>
-    </>
-  );
-}
+// export function Navigation() {
+//   return (
+//     <>
+//     <div className='navigation-container'>
+//     <nav>
+//       <ul>
+//         <li className='paginas'>
+//           <Link className='sobre-mim' to="/">Sobre mim</Link>
+//         </li>
+//         <li>
+//           <Link className='portfolio' to="/portfolio">Portfólio</Link>
+//         </li>
+//       </ul>
+//     </nav>
+//     </div>
+//     </>
+//   );
+// }
